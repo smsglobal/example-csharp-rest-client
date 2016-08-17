72957dca81f2a416d669132399dfa7f3b76bd219
Alienware <Alienware@DESKTOP-AOAT4V1>
2016-08-17 20:06:28 +1000

    Add project files.

---
+++ example-csharp-rest-client/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example_csharp_rest_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
